12342f8b59e186e088e08aa0f57258f0818c082e
Daniel Gamboa <dwtgamboa@gmail.com>
2015-05-02 20:01:10 -0700

    something happened? I don't know what

---
+++ CS241-RBTreeProject/src/edu/csupomona/cs/cs241/prog_assgnmnt_2/ParamRBTree.java
@@ -0,0 +1,28 @@
+package edu.csupomona.cs.cs241.prog_assgnmnt_2;
+
+public class ParamRBTree<V extends Comparable <V>> implements RedBlackTree<V> {
+
+	public void add() {
+		
+	}
+
+	public void remove() {
+		
+	}
+
+	public void search() {
+		
+	}
+
+	public void display() {
+		
+	}
+	
+	public class Node<V extends Comparable<V>> {
+		
+		protected V value;
+		protected ParamRBTree.COLOR color;
+		
+	}
+
+}
--- CS241-RBTreeProject/src/edu/csupomona/cs/cs241/prog_assgnmnt_2/RedBlackTree.java
+++ CS241-RBTreeProject/src/edu/csupomona/cs/cs241/prog_assgnmnt_2/RedBlackTree.java
@@ -11,4 +11,14 @@ public interface RedBlackTree<V extends Comparable<V>> {
 	public void search();
 	
 	public void display();
+	
+	/*
+	 * You are to implement a Red-Black Tree. To simplify the project, the values 
+	 * stored in the nodes will be just integers (so you don't have to worry about 
+	 * comparable types and such, or differentiating between keys and values). 
+	 * The interface for the RBT should provide the expected operations for adding, 
+	 * removing, and looking up values. In addition to that, it will support a void method 
+	 * prettyPrint() that will print to the console the values in the tree, in a pyramid fashion, 
+	 * each value printed along with its color, so as to make it easy to visualize the structure of the tree. 
+	 */
 }
