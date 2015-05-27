429bc16719430b9d8ee6063e463dfe0d1be8249c
Daniel Gamboa <dwtgamboa@gmail.com>
2015-05-27 05:38:59 -0700

    Somewhat working for testing

--- CS241-RBTreeProject/src/edu/csupomona/cs/cs241/prog_assgnmnt_2/Launcher.java
+++ CS241-RBTreeProject/src/edu/csupomona/cs/cs241/prog_assgnmnt_2/Launcher.java
@@ -1,8 +1,34 @@
 package edu.csupomona.cs.cs241.prog_assgnmnt_2;
 
+import java.util.Scanner;
+
 public class Launcher {
 
+	Tree<Integer, String> rbt = new RBTree<Integer, String>();
+	Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		
+		Launcher registry = new Launcher();
+		//K key = <Integer> Student ID
+		//V value = <String> Student Name
+
+		 ((RBTree<Integer, String>) registry.rbt).insertRB(1, "Daniel");
+		 ((RBTree<Integer, String>) registry.rbt).print(((RBTree<Integer, String>) registry.rbt).getRoot());
+	}
+	
+	public void addEntry() {
+		System.out.println("Adding...");
+		System.out.print("Enter ID#: ");
+		Integer ID = sc.nextInt();
+		sc.nextLine();
+
+		System.out.print("Enter Name: ");
+		String name = sc.nextLine();
+
+		((RBTree<Integer, String>) rbt).insertRB(ID, name);
+	}
+	
+	public void showEntry() {
+		rbt.display();
 	}
 }
